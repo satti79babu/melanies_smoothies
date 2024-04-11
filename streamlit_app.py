ae2d16d530797cf8a9d1d75b354738927a37a8e2
satti79babu <166672512+satti79babu@users.noreply.github.com>
2024-04-10 21:30:59 -0700

    Update streamlit_app.py
    upd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_dv = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -39,6 +35,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_dv = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
